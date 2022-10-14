162622f08d23ac3e3bb1e67ba7aadee7f06ebf56
silencej <owen263@gmail.com>
2022-10-14 16:11:53 +0800

    First commit

---
+++ go.mod
@@ -0,0 +1,30 @@
+module github.com/silencej/hostAlert
+
+go 1.18
+
+require (
+	github.com/spf13/viper v1.13.0
+	github.com/urfave/cli/v2 v2.19.2
+	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+	cli "github.com/urfave/cli/v2"
+
+	"golang.org/x/sys/unix"
+)
+
+type Target struct {
+	Name string `json:"name"` // E.g. slack
+	Url  string `json:"url"`
+}
+
+type Config struct {
+	Target       Target `json:"target"`
+	SpaceLimitMB uint64 `json:"spaceLimitMB"`
+}
+
+func readCfg() Config {
+
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath("$HOME/.hostAlert")
+	viper.AddConfigPath(".") // optionally look for config in the working directory
+	viper.SetDefault("SpaceLimitMB", 5*1024)
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	var content Config
+	err = viper.Unmarshal(&content)
+	if err != nil {
+		log.Panic("Fail to parseConfig: ", err)
+	}
+	return content
+}
+
+func freeSpaceOnUnix() uint64 {
+	var stat unix.Statfs_t
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	unix.Statfs(wd, &stat)
+
+	// Available blocks * size per block = available space in bytes
+	freeSpace := stat.Bavail * uint64(stat.Bsize) / 1024 / 1024 // In MB.
+	fmt.Println("Free: ", freeSpace, " MB")
+	return freeSpace
+}
+
+type SlackRequest struct {
+	Text string `json:"text"`
+}
+
+func sendMsg(cfg Config, msg string) {
+	log.Println("SendMsg to ", cfg.Target.Name)
+	slackReq := SlackRequest{
+		Text: msg,
+	}
+	jsonStr, err := json.Marshal(slackReq)
+	if err != nil {
+		log.Panic("Fail to marshal json: ", err)
+	}
+	// log.Println(jsonStr)
+	resp, err := http.Post(cfg.Target.Url, "application/json", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		log.Panic("Fail to post: ", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic("Fail to parse resp: ", err)
+	}
+	log.Println("Post resp: ", string(body))
+}
+
+func main() {
+
+	app := &cli.App{
+		Compiled: time.Now(),
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "force",
+				Aliases: []string{"f"},
+				Value:   false,
+				Usage:   "Force send notification",
+			},
+		},
+		Action: func(c *cli.Context) error {
+
+			freeSpace := freeSpaceOnUnix()
+			cfg := readCfg()
+			hostname, err := os.Hostname()
+			if err != nil {
+				log.Panic("Fail to get hostname: ", err)
+			}
+
+			if freeSpace < cfg.SpaceLimitMB {
+				sendMsg(cfg, fmt.Sprintf("Not enough space on %s: %d MB free", hostname, freeSpace))
+			}
+
+			if c.Bool("force") {
+				sendMsg(cfg, fmt.Sprintf("Space on %s: %d MB free", hostname, freeSpace))
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
